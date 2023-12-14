0447fe65796df01360f42862fd1201e9dcb73070
eyesilcodead <139955943+eyesilcodead@users.noreply.github.com>
2023-12-14 10:41:58 +0300

    Update EditForm.jsx

--- src/pages/MyFirm/EditForm.jsx
+++ src/pages/MyFirm/EditForm.jsx
@@ -81,7 +81,7 @@ function EditForm() {
             </button>
           </Alert>
         ) : null}
-
+        
         <div className="myfields">
           <div className="row imgContainer">
             <img
